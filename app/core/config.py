24f25287266eb74668b983fa570e57bec37fd356
Amitai Sopher <amitaisopher@gmail.com>
2025-11-12 20:42:02 +0200

    refactor: enhance settings configuration with environment enum and dynamic env file retrieval

--- app/core/config.py
+++ app/core/config.py
@@ -1,15 +1,29 @@
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 from pydantic import Field
 from typing import Literal
+import os
+from enum import StrEnum
 
-EnvironmentType = Literal["development", "production", "testing"]
+
+class EnvironmentType(StrEnum):
+    """Enum for application environments."""
+
+    DEVELOPMENT = "development"
+    PRODUCTION = "production"
+    STAGING = "staging"
+
+
+def get_env_file_name():
+    app_env: str = os.getenv("APP_ENV", EnvironmentType.DEVELOPMENT.value)
+    env_file = f".env.{app_env}"
+    return env_file
 
 
 class Settings(BaseSettings):
     """
     Application settings.
     """
-    environment: EnvironmentType = Field(default="development",)
+    environment: EnvironmentType = Field(default=EnvironmentType.DEVELOPMENT,)
     sentry_dsn: str | None = Field(default=None, alias="SENTRY_DSN")
     sentry_enabled: bool = Field(default=False, alias="SENTRY_ENABLED")
 
@@ -17,6 +31,10 @@ class Settings(BaseSettings):
         env_file = ".env"
         env_file_encoding = "utf-8"
         extra = "allow"
+
+    model_config = SettingsConfigDict(
+        env_file=get_env_file_name(), extra="ignore", env_file_encoding="utf-8"
+    )
 
 
 _settings = None
